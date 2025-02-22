842086e6407167340f7a03e21fc7c72e847cee3a
CBServers <serverscb@gmail.com>
2025-02-22 08:41:03 +0000

    Update artifacts

--- updater/s1x/data/ui_scripts/server_list/lobby.lua
+++ updater/s1x/data/ui_scripts/server_list/lobby.lua
@@ -1,15 +1,28 @@
 local Lobby = Lobby
 local MPLobbyOnline = LUI.mp_menus.MPLobbyOnline
 local MenuData = LUI.mp_menus.MenuData
+local MPLobbyUtils = LUI.mp_menus.MPLobbyUtils
 
 game:addlocalizedstring("LUA_MENU_SERVERLIST", "Server List")
 game:addlocalizedstring("LUA_MENU_SERVERLIST_DESC", "Browse available servers.");
 
+LeaveXboxLive = function(f5_arg0)
+	local f73_local0 = Engine.GetFirstActiveController()
+	if f73_local0 ~= nil then
+		Engine.ExecNow("xstopprivateparty", f73_local0)
+		Cac.NotifyVirtualLobby("leave_lobby", Engine.GetXUIDByController(f73_local0))
+		Engine.SetSplitScreen(false)
+		Engine.ExecNow("forcesplitscreencontrol menu_xboxlive_CLOSE", f73_local0)
+	else
+		Engine.Exec("xstopprivateparty")
+	end
+end
+
 function LeaveLobby(a1)
-	LUI.MarketingPanel.ClearViewedMessages({LUI.MarketingLocation.CaC, LUI.MarketingLocation.PlayOnline,
-											LUI.MarketingLocation.CaO})
+	LUI.MarketingPanel.ClearViewedMessages({ LUI.MarketingLocation.CaC, LUI.MarketingLocation.PlayOnline,
+		LUI.MarketingLocation.CaO })
 	LeaveXboxLive()
-	LUI.FlowManager.RequestLeaveMenuByName("menu_xboxlive", nil, true)
+	LUI.FlowManager.RequestLeaveMenuByName("menu_xboxlive_lobby", nil, true)
 end
 
 function menu_xboxlive(a1, a2)
@@ -27,20 +40,35 @@ function menu_xboxlive(a1, a2)
 		menu:AddNewItemsWidget()
 	end
 
+	-- server list button
 	local serverListButton = menu:AddButton("@LUA_MENU_SERVERLIST", function(a1, a2)
 		LUI.FlowManager.RequestAddMenu(a1, "menu_systemlink_join", true, nil)
 	end)
 	serverListButton:setDisabledRefreshRate(500)
+
+	-- private match button
+	privateMatchButton = menu:AddButton("@MENU_PRIVATE_MATCH", MPLobbyOnline.OnPrivateMatch,
+		MPLobbyOnline.disablePrivateMatchButton)
+	privateMatchButton:rename("menu_xboxlive_private_match")
+	privateMatchButton:setDisabledRefreshRate(500)
+
+	-- combat training button
+	if Engine.IsCoreMode() then
+		game:addlocalizedstring("LUA_MENU_COMBAT", "Combat Training");
+		game:addlocalizedstring("LUA_MENU_COMBAT_DESC", "Rank up offline with bots.");
+
+		local FindGameButton = menu:AddButton("@LUA_MENU_COMBAT",
+			MPLobbyOnline.OnPublicMatch, MPLobbyOnline.disablePublicMatchButton)
+		FindGameButton:rename("menu_xboxlive_find_game")
+		FindGameButton:setDisabledRefreshRate(500)
+	end
+
 	if Engine.IsCoreMode() then
 		menu:AddCACButton()
 		menu:AddCAOButton()
 		menu:AddArmoryButton()
 	end
 
-	serverListButton = menu:AddButton("@MENU_PRIVATE_MATCH", MPLobbyOnline.OnPrivateMatch,
-		MPLobbyOnline.disablePrivateMatchButton)
-	serverListButton:rename("menu_xboxlive_private_match")
-	serverListButton:setDisabledRefreshRate(500)
 	if not Engine.IsCoreMode() then
 		local leaderboardButton = menu:AddButton("@LUA_MENU_LEADERBOARD", "OpLeaderboardMain")
 		leaderboardButton:rename("OperatorMenu_leaderboard")
