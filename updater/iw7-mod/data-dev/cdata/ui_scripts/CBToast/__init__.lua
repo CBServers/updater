c5d322e16767cf6ad71a24b25860ac7b2adfb3ae
CBServers <serverscb@gmail.com>
2026-09-14 04:28:48 +0000

    Update artifacts

--- updater/iw7-mod/data-dev/cdata/ui_scripts/CBToast/__init__.lua
+++ updater/iw7-mod/data-dev/cdata/ui_scripts/CBToast/__init__.lua
@@ -1,6 +1,6 @@
 -- Non-modal toast on the LUI root, raised from C++ via ui_scripting::notify("cb_toast", { kicker, title, body })
 local TOAST_ID = "CBToast"
-local HOLD_MS = 6000
+local HOLD_MS = 8000
 local FADE_IN_MS = 150
 local FADE_OUT_MS = 300
 
